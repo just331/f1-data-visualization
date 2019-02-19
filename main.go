4cc1dc6958b2f20cf23afbca5cb3d362579fed1a
Joshua Johnson <joshuaj1397@gmail.com>
2019-02-19 16:10:58 -0600

    Added dir walk

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/joshuaj1397/f1-data-visualization/model"
+
 func main() {
+	model.InitDb()
 
 }
--- model/db.go
+++ model/db.go
@@ -1,5 +1,22 @@
 package model
 
-func init() {
+import (
+	"os"
+	"path/filepath"
+)
+
+func InitDb() {
+	var files []string
+	root := "dataset"
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
 
 }
