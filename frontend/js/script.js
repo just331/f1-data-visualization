649fa9727149492da1a9b7bbd7fbdc211127348c
Joshua Johnson <joshuaj1397@gmail.com>
2019-04-11 09:57:13 -0500

    Added worlde

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -238,6 +238,8 @@ const getResults = async (race) => {
 const drawWordle = async (drivers) => {
   var words = drivers
     .map(function(driver) {
+
+      // TODO: Refine this equation
       return {text: driver.text, size: 10 + driver.pos * 90};
   });
 
@@ -249,11 +251,31 @@ const drawWordle = async (drivers) => {
     .padding(5)
     .rotate(function() { return ~~(Math.random() * 2) * 90; })
     .font("Impact")
-    .on("end", end)
+    .on("end", draw)
     .start();
 }
 
 function end(words) { console.log(JSON.stringify(words)); }
+
+function draw(words) {
+        d3.select("#wordle").append("svg")
+                .attr("width", 850)
+                .attr("height", 350)
+                .attr("class", "wordcloud")
+                .append("g")
+                // without the transform, words words would get cutoff to the left and top, they would
+                // appear outside of the SVG area
+                .attr("transform", "translate(320,200)")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function(d) { return d.size + "px"; })
+                .style("fill", function(d, i) { return color(i); })
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+    }
 
 const ajaxRequest = async (protocol, url) => {
   const res = await fetch(url);
